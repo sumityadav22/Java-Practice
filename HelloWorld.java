23caf1c25f9a7e6400be0ee5ea5a7b4c6b72d7a4
sumityadav22 <sumityadavs5574@gmail.com>
2020-03-23 04:24:38 +0530

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+		System.out.println("Please Enter Any String..");
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		System.out. println("You entered string ->"+ s);
+
+	}
+}
