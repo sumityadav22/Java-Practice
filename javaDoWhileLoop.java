9839459bd867dbf25c1fb5bcc574f77e339c07c6
sumityadav22 <sumityadavs5574@gmail.com>
2020-03-23 04:50:30 +0530

    Java DoWhileLoop

---
+++ javaDoWhileLoop.java
@@ -0,0 +1,9 @@
+class javaDoWhileLoop{
+	public static void main(String[] args) {
+		int counter =0;
+		do{
+			System.out.println(counter);
+			counter++;
+		}while (counter<=10); /* Remeber: do-while loop carries a semicolon after while loop */ 
+	}
+}
