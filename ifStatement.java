4eb77e49037371ab8f1d57a6e2faf5d43df77a50
sumityadav22 <sumityadavs5574@gmail.com>
2020-03-23 04:36:37 +0530

    Java ifStatement

---
+++ ifStatement.java
@@ -0,0 +1,14 @@
+class ifStatement
+{
+	public static void main(String[] args) {
+		int test = 6;
+		if(test <9)
+		{
+			System.out.println("This number is less than 9");
+		}
+		else
+		{
+			System.out.println("This number is greater than 9");
+		}
+	}
+}
