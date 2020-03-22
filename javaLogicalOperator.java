296765e5531b054bbbad68998c60546266427bc3
sumityadav22 <sumityadavs5574@gmail.com>
2020-03-23 04:41:42 +0530

    Java logicalOperator

---
+++ javaLogicalOperator.java
@@ -0,0 +1,16 @@
+class javaLogicalOperator{
+	public static void main(String[] args) {
+		int a, b, c, d;
+		a = 10;
+		b = 20;
+		c = 30;
+		d = 40;
+		if (a<10 && a<6) {
+			System.out.println("Condition Satisfied !!");
+		}
+		else {
+			System.out.println("Condition is not Satisfied !!");	
+		}
+		/* similary we can use Logical OR || */
+	}
+}
