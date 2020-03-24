9a63dff13adbfd075269f4ca3cb34f2bf5eac589
sumityadav22 <sumityadavs5574@gmail.com>
2020-03-24 16:49:17 +0530

    multidimentionalArray

---
+++ multidimentionalArray.java
@@ -0,0 +1,6 @@
+class multidimentionalArray{
+	public static void main(String[] args) {
+		int firstarray[][] = {{ 8,9,10,11,15 },{12,13,14,15,16}};
+		int secondarray[][] = {{ 30,31,32,33},{43},{4,5,6}};
+	}
+}
