884b442455016006814d4dee756d4f00437cea2c
sumityadav22 <sumityadavs5574@gmail.com>
2020-04-08 03:04:12 +0530

    Java File Handling

---
+++ JavafileHandling.java
@@ -0,0 +1,36 @@
+class JavafileHandling
+{
+	public static void main(String[] args) 
+	{
+		/*
+			Java has several methods for creating, reading, updating, and deleting files.
+			The File class from the java.io package, allows us to work with files
+
+			To use the 'File' class, create an object of the class, and specify the filename or directory name
+			
+			Example :
+
+			import java.io.File;  // Import the File class
+			File myObj = new File("filename.txt"); // Specify the filename
+		*/
+
+
+
+		/*
+			The File class has many useful methods for creating and getting information about files.
+
+
+			Method				Type			Description
+			canRead()			Boolean			Tests whether the file is readable or not
+			canWrite()			Boolean			Tests whether the file is writable or not
+			createNewFile()		Boolean			Creates an empty file
+			delete()			Boolean			Deletes a file
+			exists()			Boolean			Tests whether the file exists
+			getName()			String			Returns the name of the file
+			getAbsolutePath()	String			Returns the absolute pathname of the file
+			length()			Long			Returns the size of the file in bytes
+			list()				String[]		Returns an array of the files in the directory
+			mkdir()				Boolean			Creates a directory
+		*/	
+	}
+}
