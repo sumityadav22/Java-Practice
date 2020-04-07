2cda377fac49ad0941c7cff7675ac49b51fa019f
sumityadav22 <sumityadavs5574@gmail.com>
2020-04-07 21:44:07 +0530

    Java Exception Handling

---
+++ JavaExceptions.java
@@ -0,0 +1,22 @@
+/*
+	Exception handling is the process of responding to exceptions when a computer program runs
+*/
+
+	
+class JavaExceptions
+{
+	public static void main(String[] args) 
+	{
+		// We use try block to catch the error
+		try 
+		{
+	      int[] myNumbers = {1, 2, 3};
+	      System.out.println(myNumbers[10]);
+	    }
+	    // we use catch to execute some code to handle it
+	    catch (Exception e) 
+	    {
+	      System.out.println("Something went wrong.");
+	    }
+	}
+}
